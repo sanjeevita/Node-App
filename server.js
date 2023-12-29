5b4864e6d0cec17ed09036dd076311fe1e952e8d
sanjeevita <sanjeevita.sengupta2021@vitstudent.ac.in>
2023-12-30 01:09:18 +0530

    server using express

--- server.js
+++ server.js
@@ -1,55 +1,43 @@
-const http = require("http");  
+const express = require('express')
+const app = express()
+app.use(express.json());
+const port=8081
+const todos = ['js','react','html']
 
-const port = 8081; 
-
-const toDoList = ["java", "javaScript", "Python"];
-
-http.createServer((req, res)=>{
-    const {method, url} = req;
-
-    if(url === '/todos'){
-        if(method === 'GET'){
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.write(toDoList.toString());
-        }else if (method === 'POST'){
-            let body = "";
-            req.on('error',(err)=>{
-                console.log(err);
-            }).on('data', (chunk)=>{
-                body += chunk;
-            }).on('end', ()=>{
-                body=JSON.parse(body)
-                let newToDo = toDoList;
-                newToDo.push(body.item);
-                console.log(newToDo);
-            })
-        } else if (method === 'DELETE'){
-            let body = '';
-            req.on('error', (err)=>{
-                console.log(err)
-            }).on('data', (chunk)=>{
-                body+= chunk
-            }).on('end', ()=>{
-                body = JSON.parse(body);
-                let deleteThis = body.item;
-
-                toDoList.find((elem, index)=>{
-                    if(elem === deleteThis ){
-                        toDoList.splice(index, 1);
-                    }
-                })
-            })
-        }
-        
-
-    } else if (url === '/'){
-        res.writeHead(200, {"Content-Type": "text/html"} );
-        res.write("<h1>Default Page</h1>");
-    }else{
-        res.writeHead(501);
-    }
-    res.end();
+app.get("/todos",(req,res)=>{
+    res.status(200).send(todos)
 })
-.listen(port, ()=>{
-    console.log(`Nodejs Server started running on port ${port}`);
-});
+
+app.post("/todos", (req,res)=>{
+    const newtodo = req.body.item;
+    todos.push(newtodo);
+    res.status(201).send({
+        message : "task added successfully",
+        value : newtodo
+    })
+})
+
+app.delete("/todos",(req,res)=>{
+    const taskToDelete = req.body.item;
+    todos.find((item,i)=>{
+        if(item===taskToDelete){
+            todos.splice(i,1);
+        }
+    })
+    res.status(202).send({
+        message : "task deleted successfully",
+        updated : todos
+    })
+})
+
+app.all("/todos",(req,res)=>{
+    res.status(501).send("page not found")
+})
+
+app.all("*",(req,res)=>{
+    res.status(501).send("page not found")
+})
+
+app.listen(port, ()=>{
+    console.log('server started on port 8081')
+})
---
+++ using-node.js
@@ -0,0 +1,55 @@
+const http = require("http");  
+
+const port = 8080; 
+
+const toDoList = ["java", "javaScript", "Python"];
+
+http.createServer((req, res)=>{
+    const {method, url} = req;
+
+    if(url === '/todos'){
+        if(method === 'GET'){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(toDoList.toString());
+        }else if (method === 'POST'){
+            let body = "";
+            req.on('error',(err)=>{
+                console.log(err);
+            }).on('data', (chunk)=>{
+                body += chunk;
+            }).on('end', ()=>{
+                body=JSON.parse(body)
+                let newToDo = toDoList;
+                newToDo.push(body.item);
+                console.log(newToDo);
+            })
+        } else if (method === 'DELETE'){
+            let body = '';
+            req.on('error', (err)=>{
+                console.log(err)
+            }).on('data', (chunk)=>{
+                body+= chunk
+            }).on('end', ()=>{
+                body = JSON.parse(body);
+                let deleteThis = body.item;
+
+                toDoList.find((elem, index)=>{
+                    if(elem === deleteThis ){
+                        toDoList.splice(index, 1);
+                    }
+                })
+            })
+        }
+        
+
+    } else if (url === '/'){
+        res.writeHead(200, {"Content-Type": "text/html"} );
+        res.write("<h1>Default Page</h1>");
+    }else{
+        res.writeHead(501);
+    }
+    res.end();
+})
+.listen(port, ()=>{
+    console.log(`Nodejs Server started running on port ${port}`);
+});
